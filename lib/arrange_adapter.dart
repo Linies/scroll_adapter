91c23482b68b89e7e8afc73741cb771f814ff4b9
liniec <1260497591@qq.com>
2022-03-13 19:09:14 +0800

    feat: adapter&view业务层接口实现

--- lib/arrange_adapter.dart
+++ lib/arrange_adapter.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/widgets.dart';
 import 'package:scroll_adapter/core/base_impl.dart';
 
+/// 滑动适配器业务层接口
 abstract class ArrangeAdapter<E> extends DataBuildAdapter<E> {
   ArrangeAdapter({DataBuildState? state}) : super(state: state);
 }
 
-abstract class ArrangeListView extends StatefulWidget {
+/// 索引项构建以回调方式给外部的滑动生成函数
+typedef ScrollWithIndexedBuilder = Widget Function(IndexedWidgetBuilder indexedWidgetBuilder);
+
+/// 外部继承独立实现滑动视图的可适配类
+/// [buildScroll]外部在该方法实现具体滑动视图
+abstract class AdaptableScroll extends StatelessWidget {
+
   final ArrangeAdapter adapter;
 
-  ArrangeListView(this.adapter);
+  AdaptableScroll(this.adapter);
+
+  Widget buildScroll(IndexedWidgetBuilder indexedWidgetBuilder);
 
   @override
-  ArrangeListViewState createState() => ArrangeListViewState();
+  Widget build(BuildContext context) => _ArrangeView(adapter, (builder) => buildScroll(builder));
 }
 
-/// todo:
-class ArrangeListViewState extends DataBuildState<ArrangeListView>
+class _ArrangeView extends StatefulWidget {
+  final ArrangeAdapter adapter;
+
+  final ScrollWithIndexedBuilder scrollvIdxBuilder;
+
+  _ArrangeView(this.adapter, this.scrollvIdxBuilder);
+
+  @override
+  _ArrangeState createState() => _ArrangeState();
+}
+
+class _ArrangeState extends DataBuildState<_ArrangeView>
     with DataBuildBase {
 
   @override
@@ -25,9 +44,5 @@ class ArrangeListViewState extends DataBuildState<ArrangeListView>
   }
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemBuilder: (item, position) =>
-            widget.adapter.bindItemView(item, position));
-  }
+  Widget build(BuildContext context) => widget.scrollvIdxBuilder(widget.adapter.bindItemView);
 }
--- lib/scroll_adapter.dart
+++ lib/scroll_adapter.dart
@@ -1,4 +1,3 @@
 library scroll_adapter;
 
-export 'package:scroll_adapter/arrange_adapter.dart';
-export 'package:scroll_adapter/core/base_impl.dart' show DataBuildAdapter;
+export 'package:scroll_adapter/arrange_adapter.dart' show ArrangeAdapter, AdaptableScroll;
